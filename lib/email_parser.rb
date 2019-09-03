71c74069a5d79fbac6475d956d70137bb95faab7
David Strauch <dmaster18@gmail.com>
2019-09-03 17:02:09 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -20,4 +20,4 @@ end
 email_addresses = "john@doe.com, person@somewhere.org,, , dman@gmail.com"
 parser = EmailAddressParser.new(email_addresses)
 parser.parse
-puts parser.
+puts parser.email_address_array
