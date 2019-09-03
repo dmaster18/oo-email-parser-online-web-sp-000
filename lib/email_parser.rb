f6a36d1da4a72a0d4e642a214a5bb10f0c669837
David Strauch <dmaster18@gmail.com>
2019-09-03 17:03:09 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,6 +11,8 @@ class EmailAddressParser
   
   def parse
     @email_address_array = @email_addresses.split(/[\s,]/)
+        @email_address_array.delete("")
+    @email_address_array.delete(" ")
     @email_address_array
   end
 end
