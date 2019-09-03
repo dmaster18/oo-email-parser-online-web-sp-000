ac8107f62383f9be871d1d6d76dd318879592f32
David Strauch <dmaster18@gmail.com>
2019-09-03 17:07:12 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,7 +14,7 @@ class EmailAddressParser
     @email_address_array.delete("")
     @email_address_array.delete(" ")
     i = 0 
-    
+    @comparative_array
     while i < @email_address_array.length
       
   end
